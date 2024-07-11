4b3bcbc69c9f8d6174cde0ec07c9e4e77fd87996
Zed-bard <zerihunbard@gmail.com>
2024-07-11 12:58:52 -0700

    3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,14 @@
--- Lists all bands with Glam rock as their main style, ranked by their longevity.
--- SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
-SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
-    FROM metal_bands
-    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-    ORDER BY lifespan DESC;
+USE holberton;
+
+SELECT 
+    name AS band_name, 
+    IFNULL(
+        2022 - IFNULL(split, 2022), 
+        0
+    ) AS lifespan
+FROM 
+    bands
+WHERE 
+    style = 'Glam rock'
+ORDER BY 
+    lifespan DESC;
