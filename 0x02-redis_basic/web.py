c06d32027fca1ea0490c27d0e28caaf1262ddfab
root <zerihunbard@gmail.com>
2024-07-17 11:25:25 -0700

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -19,22 +19,13 @@ def data_cacher(method: Callable) -> Callable:
     def invoker(url: str) -> str:
         '''The wrapper function for caching the output.
         '''
-        # Increment the URL access count
         redis_store.incr(f'count:{url}')
-        
-        # Check if the result is already cached
-        cached_result = redis_store.get(f'result:{url}')
-        if cached_result:
-            return cached_result.decode('utf-8')
-        
-        # Fetch the result from the URL
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
         result = method(url)
-        
-        # Cache the result with an expiration time of 10 seconds
         redis_store.setex(f'result:{url}', 10, result)
-        
         return result
-    
     return invoker
 
 
@@ -43,6 +34,4 @@ def get_page(url: str) -> str:
     '''Returns the content of a URL after caching the request's response,
     and tracking the request.
     '''
-    response = requests.get(url)
-    response.raise_for_status()  # Ensure we handle HTTP errors
-    return response.text
+    return requests.get(url).text
